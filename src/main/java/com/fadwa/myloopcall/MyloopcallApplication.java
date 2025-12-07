da030316da0a51e031f4a56f16218201717df93c
Youssef Hihi <Youssef.Hihi@progressoft.com>
2025-12-07 20:42:32 +0100

    first commit

---
+++ src/main/java/com/fadwa/myloopcall/MyloopcallApplication.java
@@ -0,0 +1,13 @@
+package com.fadwa.myloopcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloopcallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyloopcallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fadwa/myloopcall/MyloopcallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadwa.myloopcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyloopcallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
